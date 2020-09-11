fb98d2f0b87613698a5f96187eb8e66a8b6e299a
shawnm7474 <shawnm74@gmail.com>
2020-09-10 21:45:20 -0500

    additional changes to generate readme

--- Develop/README.md
+++ Develop/README.md
@@ -1,21 +1,39 @@
+
 # README_generator
-## Description
+
+# Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Credits](#credits)
+- [Questions](#questions)
+
+## Description:
+Mozilla Public License Version 2.0
 A command-line application will allow for quick and easy generation of a project README to get started quickly. This will allow a project creator to spend more time working on finishing the project and less time creating a good README.
-## Table of Contents
-- Installation,Usage,License,Contributing,Tests,Questions
-## Install Instructions
+
+## Installation:
 Clone repo, install dependencies with 'npm install' command in terminal interface, then run the program with the command 'node index.js'.
-## Usage
-remove this question
-## License
-MIT License,Apache License,Public unlicensed
-## How to Contribute
-Contact me via e-mail
-## How to run tests
-Once installed, issue 'npm run test' in terminal
-function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    }
+
+## Usage:
+When starting a new project, use this to generate a README.md file quickly and in a standard format
+
+## License:
+    Click the link below for more information
+    -[License](https://opensource.org/licenses/Mozilla Public License Version 2.0)
+
+## Contributing:
+Contact me via e-mail 
+
+## Test:
+    Once installed, issue 'npm run test' in terminal
+
+## Questions:
+    Please visit my github page or e-mail me directly to contact me about this project
+    -[github](https://github/com/Shawn-Morgan)
+    -shawnm74@gmail.com
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,136 +1,123 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-// array of questions for user
-const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "What is your project's title?"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "What is your project's description in brief?"
-    },
-    {
-        type: "input",
-        name: "toc",
-        message: "What is the table of contents (separate with commas)?"
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "What install guidance do you have?"
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "Provide instructions and examples for use."
-    },
-    {
-        type: "checkbox",
-        name: "license",
-        message: "What license should this be attributed too?",
-        choices: [
-            "MIT License",
-            "Apache License",
-            "Public unlicensed" 
-        ]},
-    {
-        type: "input",
-        name: "contributing",
-        message: "How should someone contribute?"
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "What tests have been conducted?"
-    },
-    {
-        type: "input",
-        name: "questions",
-        message: "If there are questions, to whom should they reach out too? Enter e-mail"
-    },
-]
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//questions for user
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is your project's title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "What is your project's description in brief?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What install guidance do you have?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Provide instructions and examples for use."
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "What license should this be attributed too?",
+            choices: [
+                "Affero GPL",
+                "Artistic License 2.0",
+                "BSD 3-Clause License",
+                "BSD 2-Clause license",
+                "Eclipse Public License v1.0",
+                "GPL v3",
+                "LGPL v2.1",
+                "LGPL v3",
+                "Mozilla Public License Version 2.0",
+                "Public Domain (Unlicense)",
+            ]},
+        {
+            type: "input",
+            name: "contributing",
+            message: "How should someone contribute?"
+        },
+        {
+            type: "input",
+            name: "githubusername",
+            message: "Enter your github username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "If there are questions, please include e-mail: "
+        },
+    ]);
+}
 console.clear();
 
-
 // function to write README file
-    //function writeToFile(fileName, data) {
-    //}
+function writeReadMe(response) {
+    return `
+# ${response.title}
 
-inquirer.prompt(questions).then(response => {
+# Table of Contents
 
-    fs.appendFileSync("README.md", ("# " + response.title) + '\n', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    });
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Credits](#credits)
+- [Questions](#questions)
 
-    fs.appendFileSync("README.md", ("## Description" + '\n' + response.description) + '\n', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    });
+# Description:
+${response.license}
+${response.description}
 
-    fs.appendFileSync("README.md", ("## Table of Contents" + '\n' + '- ' + response.toc.split(", ").
-    join('\n' + '- ')) + '\n', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    });
+# Installation:
+${response.installation}
 
-    fs.appendFileSync("README.md", ("## Install Instructions" + '\n' + response.installation) + '\n', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    });
+# Usage:
+${response.usage}
 
-    fs.appendFileSync("README.md", ("## Usage" + '\n' + response.usage) + '\n', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    });
+# License:
+    Click the link below for more information
+    - [License](https://opensource.org/licenses/${response.license})
 
-    fs.appendFileSync("README.md", ("## License" + '\n' + response.license) + '\n', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    });
+# Contributing:
+${response.contributing}
 
-    fs.appendFileSync("README.md", ("## How to Contribute" + '\n' + response.contributing) + '\n', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    });
+# Test:
+    Once installed, issue 'npm run test' in terminal
 
-    fs.appendFileSync("README.md", ("## How to run tests" + '\n' + response.tests) + '\n', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    });
-
-    fs.appendFileSync("README.md", ('\n' + response.questions + '\n', function(err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("done");
-    }));
-});
+# Questions:
+    Please visit my github page or e-mail me directly to contact me about this project
+    - [github](https://github/com/${response.githubusername})
+    - ${response.email}
+    `;
+}
 
 // function to initialize program
-//function init() {
-//}
+async function init() {
+    try {
+        const response = await promptUser();
+
+        const readMe = writeReadMe(response);
+
+        await writeFileAsync("README.md", readMe);
+        console.log("done");
+    } catch(err) {
+        console.log(err);
+    }
+}
 
 // function call to initialize program
-//init();
+init();
